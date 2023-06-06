490412392fac1480fb0a22f80ad9717bb3fa885b
davidcoats <david@getsafetycone.com>
2023-06-06 12:32:25 -0700

    Initial commit.

---
+++ source/R5T.S0072/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0072
+{
+	/// <summary>
+	/// Code element (project, solution, solution set, library) specification generation scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
